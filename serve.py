143f0a665824ab14262f76803d778504b28fd823
zabir-nabil <zabiralnazi@yahoo.com>
2021-12-31 02:54:31 +0600

    added src

---
+++ serve.py
@@ -0,0 +1,62 @@
+from fastapi import Request, FastAPI, File, UploadFile
+from fastapi.responses import FileResponse
+import base64
+import uuid
+import uvicorn
+import numpy as np
+import cv2
+import torch
+
+app = FastAPI()
+app.model = torch.load('../best_model.pth', map_location=torch.device('cpu'))
+
+def preprocess_input_smp(x, mean=None, std=None, input_space="RGB", input_range=None, **kwargs):
+    if input_space == "BGR":
+        x = x[..., ::-1].copy()
+    if input_range is not None:
+        if x.max() > 1 and input_range[1] == 1:
+            x = x / 255.0
+    if mean is not None:
+        mean = np.array(mean)
+        x = x - mean
+    if std is not None:
+        std = np.array(std)
+        x = x / std
+    return x
+
+def to_tensor(x, **kwargs):
+    return x.transpose(2, 0, 1).astype('float32')
+
+@app.get("/")
+async def index():
+    return {"msg": "server is running."}
+
+@app.post("/upload")
+async def post_base64Image(request: Request):
+    imgstr = await request.json()
+    imgdata = base64.b64decode(imgstr.get("base64_image"))
+    filename = f"dumped/img_{uuid.uuid4().hex}.png"
+    with open(filename, 'wb') as f:
+        f.write(imgdata)
+
+    image = cv2.imread(filename)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.resize(image, (480, 480))
+
+    image = preprocess_input_smp(image, input_space='RGB', input_range=[0, 1], mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    image = to_tensor(image)
+
+    x_tensor = torch.from_numpy(image).to('cpu').unsqueeze(0)
+    pr_mask = app.model.predict(x_tensor)
+    pr_mask = (pr_mask.squeeze().cpu().numpy().round())
+
+    pr_mask = (pr_mask * 255.0).astype(np.uint8)
+
+    _, encoded_img = cv2.imencode('.PNG', pr_mask)
+
+    encoded_img = base64.b64encode(encoded_img)
+    
+    return {'mask': encoded_img}
+
+if __name__ == '__main__':
+    uvicorn.run("serve:app", port=80, host='0.0.0.0', reload = True)
