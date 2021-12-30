6e55bfad796adf71f7165fa1e394fc301c61d871
zabir-nabil <zabiralnazi@yahoo.com>
2021-12-31 04:03:03 +0600

    added src

--- serve.py
+++ serve.py
@@ -34,29 +34,48 @@ async def index():
 @app.post("/upload")
 async def post_base64Image(request: Request):
     imgstr = await request.json()
-    imgdata = base64.b64decode(imgstr.get("base64_image"))
-    filename = f"dumped/img_{uuid.uuid4().hex}.png"
-    with open(filename, 'wb') as f:
-        f.write(imgdata)
 
-    image = cv2.imread(filename)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    image = cv2.resize(image, (480, 480))
+    try:
+        imgdata = base64.b64decode(imgstr.get("base64_image"))
+        filename = f"dumped/img_{uuid.uuid4().hex}.png"
+        with open(filename, 'wb') as f:
+            f.write(imgdata)
 
-    image = preprocess_input_smp(image, input_space='RGB', input_range=[0, 1], mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    image = to_tensor(image)
+        image = cv2.imread(filename)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = cv2.resize(image, (480, 480))
 
-    x_tensor = torch.from_numpy(image).to('cpu').unsqueeze(0)
-    pr_mask = app.model.predict(x_tensor)
-    pr_mask = (pr_mask.squeeze().cpu().numpy().round())
+        image = preprocess_input_smp(image, input_space='RGB', input_range=[0, 1], mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        image = to_tensor(image)
 
-    pr_mask = (pr_mask * 255.0).astype(np.uint8)
+        x_tensor = torch.from_numpy(image).to('cpu').unsqueeze(0)
+        pr_mask = app.model.predict(x_tensor)
+        pr_mask = (pr_mask.squeeze().cpu().numpy().round())
 
-    _, encoded_img = cv2.imencode('.PNG', pr_mask)
+        pr_mask = (pr_mask * 255.0).astype(np.uint8)
 
-    encoded_img = base64.b64encode(encoded_img)
-    
-    return {'mask': encoded_img}
+        if imgstr.get("mask_type") == "rgba":
+            rgb = cv2.merge((pr_mask,pr_mask,pr_mask))
+            rgba = cv2.cvtColor(rgb, cv2.COLOR_RGB2RGBA)
+
+            rgba[:, :, 3] = pr_mask
+
+            _, encoded_img = cv2.imencode('.PNG', rgba)
+            encoded_img = base64.b64encode(encoded_img)
+            return {'mask': encoded_img}
+        elif imgstr.get("mask_type") == "single_channel":
+            _, encoded_img = cv2.imencode('.PNG', pr_mask)
+            encoded_img = base64.b64encode(encoded_img)
+            return {'mask': encoded_img}
+        else:
+            rgb = cv2.merge((pr_mask,pr_mask,pr_mask))
+            _, encoded_img = cv2.imencode('.PNG', rgb)
+            encoded_img = base64.b64encode(encoded_img)
+            return {'mask': encoded_img}
+
+
+    except:
+        return {'msg': 'mask generation failed'}
 
 if __name__ == '__main__':
     uvicorn.run("serve:app", port=80, host='0.0.0.0', reload = True)
